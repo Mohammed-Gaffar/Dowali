3b6542058b0a8e376f9a4e8238d99bee4033c0da
mohammed - gaffar <the-100joker@live.com>
2024-07-20 15:26:49 +0300

    updates

---
+++ Dowali.UI/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PlayApp.Extentions;
+
+namespace Dowali.UI.Controllers
+{
+    public class AuthController : BaseController
+    {
+        private readonly IConfiguration _configuration;
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            foreach (var cookie in Request.Cookies.Keys)
+            {
+                Response.Cookies.Delete(cookie);
+            }
+
+            return Redirect(_configuration.GetSection("FormsAuthentication:Logout").Value);
+        }
+
+        [Authorize]
+        public IActionResult Login()
+        {
+            return RedirectToAction("Index", "Services");
+        }
+    }
+}
